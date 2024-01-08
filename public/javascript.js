0ba6d3e57491f298f7ff119836568ed0e9480b03
MK-56 <155998658+MK-56@users.noreply.github.com>
2024-01-08 12:35:05 -0500

    Update javascript.js

--- public/javascript.js
+++ public/javascript.js
@@ -5,9 +5,9 @@ var user = cookie.get('user');
 if (!user) {
 
   // Ask for the username if there is none set already
-  user = prompt('Choose a username:');
+  user = prompt('Username? Click cancel for Anonymous.:');
   if (!user) {
-    alert('We cannot work with you like that!');
+    alert('Going as Anonymous');
   } else {
     // Store it in the cookies for future use
     cookie.set('user', user);
